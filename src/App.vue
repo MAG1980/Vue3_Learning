fd629bc715bd43e058fb9f488f85456e742fc4de
Alexey Medvedev <76469986+MAG1980@users.noreply.github.com>
2022-01-28 20:57:08 +0300

    First commit First commit

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
+  <div>
+    <div>
+      Количество лайков: <strong>{{ likes }}</strong>
+    </div>
+    <div>
+      Количество дизлайков: <strong>{{ dislikes }}</strong>
+    </div>
+  </div>
 </template>
-
 <script>
-import HelloWorldVue from "./components/HelloWorld.vue";
 export default {
-  name: "App",
-  components: {
-    HelloWorld: HelloWorldVue,
+  data() {
+    return {
+      likes: 0,
+      dislikes: 5,
+    };
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
