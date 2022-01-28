21b62ee54cdfb24ce18cb5826e38a4484c4df7fa
Alexey Medvedev <76469986+MAG1980@users.noreply.github.com>
2022-01-28 21:56:05 +0300

    Вывод постов из массива Организовал вывод постов из массива объектов. Добавил форму для добавления постов.

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,39 @@
 <template>
-  <div>
-    <div class="post">
-      <div><strong>Название:</strong> Пост о JavaScript</div>
-      <div><strong>Описание:</strong> Текст поста о JavaScript</div>
-      <div class="post__buttons">
-        <button class="post__button post__button_like" v-on:click="addLike">
-          Like
-        </button>
-        <button
-          class="post__button post__button_dislike"
-          v-on:click="addDislike"
-        >
-          DisLike
-        </button>
-      </div>
-      <div>
-        Количество лайков: <strong>{{ likes }}</strong>
-      </div>
-      <div>
-        Количество дизлайков: <strong>{{ dislikes }}</strong>
-      </div>
-    </div>
-    <div class="post">
-      <div><strong>Название:</strong> Пост о JavaScript</div>
-      <div><strong>Описание:</strong> Текст поста о JavaScript</div>
-      <div class="post__buttons">
-        <button class="post__button post__button_like" v-on:click="addLike">
-          Like
-        </button>
-        <button
-          class="post__button post__button_dislike"
-          v-on:click="addDislike"
-        >
-          DisLike
-        </button>
-      </div>
-      <div>
-        Количество лайков: <strong>{{ likes }}</strong>
-      </div>
-      <div>
-        Количество дизлайков: <strong>{{ dislikes }}</strong>
-      </div>
-    </div>
-    <div class="post">
-      <div><strong>Название:</strong> Пост о JavaScript</div>
-      <div><strong>Описание:</strong> Текст поста о JavaScript</div>
-      <div class="post__buttons">
-        <button class="post__button post__button_like" v-on:click="addLike">
-          Like
-        </button>
-        <button
-          class="post__button post__button_dislike"
-          v-on:click="addDislike"
-        >
-          DisLike
-        </button>
-      </div>
-      <div>
-        Количество лайков: <strong>{{ likes }}</strong>
-      </div>
-      <div>
-        Количество дизлайков: <strong>{{ dislikes }}</strong>
+  <div class="app">
+    <form class="post__form">
+      <h4>Создание поста</h4>
+      <input class="post__input" type="text" placeholder="Название поста" />
+      <input class="post__input" type="text" placeholder="Содержание поста" />
+      <button class="post__button" type="submit">Добавить пост</button>
+    </form>
+
+    <div v-for="post in posts" v-bind:key="post.id">
+      <div class="post">
+        <div>
+          <strong>{{ post.title }}:</strong>
+        </div>
+        <div>{{ post.content }}</div>
+        <div class="post__buttons">
+          <div>
+            <button class="post__button post__button_like" v-on:click="addLike">
+              Like
+            </button>
+            <div>
+              Количество лайков: <strong>{{ likes }}</strong>
+            </div>
+          </div>
+          <div>
+            <button
+              class="post__button post__button_dislike"
+              v-on:click="addDislike"
+            >
+              DisLike
+            </button>
+            <div>
+              Количество дизлайков: <strong>{{ dislikes }}</strong>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -71,6 +44,23 @@ export default {
     return {
       likes: 0,
       dislikes: 5,
+      posts: [
+        {
+          id: 1,
+          title: "JavaScript 1",
+          content: "Содержание поста о JavaScript 1",
+        },
+        {
+          id: 2,
+          title: "JavaScript 2",
+          content: "Содержание поста о JavaScript 2",
+        },
+        {
+          id: 3,
+          title: "JavaScript 3",
+          content: "Содержание поста о JavaScript 3",
+        },
+      ],
     };
   },
   methods: {
@@ -89,9 +79,13 @@ export default {
   padding: 0;
   box-sizing: border-box;
 }
+.app {
+  padding: 20px;
+}
 .post {
   padding: 15px;
   border: 3px solid teal;
+  margin-bottom: 10px;
 }
 .post__buttons {
   display: flex;
@@ -99,11 +93,24 @@ export default {
 }
 .post__button {
   padding: 5px 10px;
+  margin: 10px 0;
 }
 .post__button_like {
   background: teal;
 }
 .post__button_dislike {
   background: red;
+}
+.post__form {
+  display: flex;
+  flex-direction: column;
+  border: 3px solid teal;
+  margin-bottom: 10px;
+  padding: 10px;
+}
+.post__input {
+  border: 1px solid teal;
+  padding: 5px;
+  margin-top: 10px;
 }
 </style>
