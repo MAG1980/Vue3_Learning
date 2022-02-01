e48664db7df99f723d86aa44301a5cac71624f42
Alexey Medvedev <76469986+MAG1980@users.noreply.github.com>
2022-02-02 01:27:04 +0300

    Добавлена анимация списка постов Используется нативный глобальный компонент Vue: transition-group.

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="box" v-show="posts.length">
     <h3 class="title">Список постов</h3>
-    <PostItem
-      v-for="post in posts"
-      :key="post.id"
-      :post="post"
-      @remove="$emit('remove', post)"
-    />
+    <transition-group name="postList">
+      <PostItem
+        v-for="post in posts"
+        :key="post.id"
+        :post="post"
+        @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
   <div v-show="!posts.length">
     <h3 class="title title_error">Список постов пуст</h3>
@@ -38,5 +40,16 @@ export default {
 }
 .title_error {
   color: red;
+}
+.postList-move,
+.postList-enter-active,
+.postList-leave-active {
+  transition: all 0.5s ease;
+}
+
+.postList-enter-from,
+.postList-leave-to {
+  opacity: 0;
+  transform: translateX(100px);
 }
 </style>
