c52916edc2538bf8743ccf526c3c2f25d2e359de
Alexey Medvedev <76469986+MAG1980@users.noreply.github.com>
2022-02-06 15:20:57 +0300

    Добавлен маршрут к странице постов

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import components from "@/components/UI";
 import App from "./App.vue";
-import router from "@/router/router.js";
+import { router } from "@/router/router.js";
 const app = createApp(App);
 components.forEach((component) => {
   app.component(component.name, component);
--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,15 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Main from "@/pages/Main";
+import PostPage from "@/pages/PostPage";
 
 const routes = [
   {
     path: "/",
     component: Main
+  },
+  {
+    path: "/posts",
+    component: PostPage
   }
 ];
 
