181dd67bcb3d14c359fba30fbec2536cb6f59cc3
Alexey Medvedev <76469986+MAG1980@users.noreply.github.com>
2022-02-07 22:11:55 +0300

    Добавил получение данных поста с сервера В компоненте PostItemPage делаю запрос на сервер по id страницы, полученному из адресной строки браузера.

--- src/pages/PostItemPage.vue
+++ src/pages/PostItemPage.vue
@@ -1,8 +1,57 @@
 <template>
-  <h1>Страница поста {{ $route.params.id }}</h1>
+  <h2 class="post__index">Страница поста {{ $route.params.id }}</h2>
+  <h1 class="post__title">{{ post.title }}</h1>
+  <p class="post__body">{{ post.body }}</p>
 </template>
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      isPostLoading: false,
+      post: "",
+    };
+  },
+  methods: {
+    async fetchPost() {
+      this.isPostLoading = true;
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts",
+          {
+            params: {
+              id: this.$route.params.id,
+            },
+          }
+        );
+        this.post = response.data[0];
+      } catch (e) {
+        console.log(e);
+      } finally {
+        this.isPostLoading = false;
+      }
+    },
+  },
+  mounted() {
+    this.fetchPost();
+  },
+};
 </script>
 <style scoped>
+.post__title {
+  color: blue;
+  text-align: center;
+  margin-bottom: 15px;
+  text-transform: uppercase;
+}
+.post__index {
+  color: teal;
+  margin-bottom: 5px;
+  font-size: 12px;
+}
+.post__body::first-letter {
+  text-transform: uppercase;
+  font-size: 30px;
+  color: red;
+}
 </style>
